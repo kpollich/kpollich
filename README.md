984094f5c2528e8abf86455fa9522b557b26b64c
Kyle Pollich <kpollich1@gmail.com>
2020-07-21 10:36:23 -0400

    Add recent artists

--- README.md
+++ README.md
@@ -9,3 +9,46 @@ I work at [Shift Lab](https://shiftlab.co/) as a Lead Developer.
 
 For more, check out my personal site: https://kylepollich.com/
 
+## What I'm listening to lately
+
+<!-- begin artists -->
+<style>
+  #artists {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 160px);
+    grid-gap: 15px;
+    justify-items: start;
+  }
+
+  #artists div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  #artists img {
+    height: 160px;
+    width: 160px;
+  }
+</style>
+
+<div id="artists">
+  <div>
+    <img src="https://i.scdn.co/image/3b6a427f0c54c0d116c433462ae1dd48474643d0" alt="Phoebe Bridgers" />
+    <a href="https://open.spotify.com/artist/1r1uxoy19fzMxunt3ONAkG">Phoebe Bridgers</a>
+  </div><div>
+    <img src="https://i.scdn.co/image/cbed180a43a152df83d00d04bec789ca4c62ea7c" alt="Pinegrove" />
+    <a href="https://open.spotify.com/artist/2gbT6GPXMis0OAkZbEQCYB">Pinegrove</a>
+  </div><div>
+    <img src="https://i.scdn.co/image/e803cdc6e5a109c3fbb7e0b3cd9a63a06c001e9c" alt="Christian Lee Hutson" />
+    <a href="https://open.spotify.com/artist/5B7NeaqVrmXPyF05C9tnZ3">Christian Lee Hutson</a>
+  </div><div>
+    <img src="https://i.scdn.co/image/aef4c8f3992a5ddb727bd0468854d7e1047a8851" alt="The Midnight" />
+    <a href="https://open.spotify.com/artist/2NFrAuh8RQdQoS7iYFbckw">The Midnight</a>
+  </div><div>
+    <img src="https://i.scdn.co/image/75bb29e9852e14d6e8495950a7d20b7715a919c5" alt="Carly Rae Jepsen" />
+    <a href="https://open.spotify.com/artist/6sFIWsNpZYqfjUpaCgueju">Carly Rae Jepsen</a>
+  </div>
+</div>
+<!-- end artists -->
---
+++ scripts/get-recently-played.js
@@ -0,0 +1,131 @@
+require("dotenv").config();
+
+const axios = require("axios");
+const qs = require("query-string");
+const fs = require("fs");
+const path = require("path");
+const { html } = require("common-tags");
+
+const {
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  SPOTIFY_REFRESH_TOKEN,
+} = process.env;
+
+main()
+  .then(() => console.log("Done"))
+  .catch((error) => console.error(error));
+
+async function main() {
+  const token = await getAccessToken();
+  console.log(`Got access token: ${token}`);
+
+  const topArtists = await getTopArtists(token);
+  console.log("Got artists data");
+
+  console.log("Generating HTML...");
+  const html = generateArtistHtml(topArtists.items);
+
+  const readme = await (
+    await fs.promises.readFile(path.join(__dirname, "../README.md"))
+  ).toString();
+
+  const newReadme = readme.replace(
+    /\<\!\-\- begin artists \-\-\>(.|\s)*\<\!\-\- end artists \-\-\>/gi,
+    html
+  );
+
+  await fs.promises.writeFile(path.join(__dirname, "../README.md"), newReadme);
+}
+
+async function getAccessToken() {
+  const url = "https://accounts.spotify.com/api/token";
+  const body = qs.stringify({
+    grant_type: "refresh_token",
+    refresh_token: SPOTIFY_REFRESH_TOKEN,
+  });
+
+  const authHeader = getAuthHeader();
+
+  const response = await axios.post(url, body, {
+    headers: {
+      Authorization: authHeader,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const { access_token: token } = response.data;
+
+  return token;
+}
+
+function getAuthHeader() {
+  const token = Buffer.from(
+    `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+  ).toString("base64");
+  const authHeader = `Basic ${token}`;
+
+  return authHeader;
+}
+
+async function getTopArtists(token) {
+  const url = "https://api.spotify.com/v1/me/top/artists?time_range=short_term";
+
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+function generateArtistHtml(artists) {
+  let artistsHtml = "";
+
+  for (const artist of artists.slice(0, 5)) {
+    const name = artist.name;
+    const url = artist.external_urls.spotify;
+    const image = artist.images[artist.images.length - 1].url;
+
+    artistsHtml += html`
+      <div>
+        <img src="${image}" alt="${name}" />
+        <a href="${url}">${name}</a>
+      </div>
+    `;
+  }
+
+  const style = html`
+    <style>
+      #artists {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 160px);
+        grid-gap: 15px;
+        justify-items: start;
+      }
+
+      #artists div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+      }
+
+      #artists img {
+        height: 160px;
+        width: 160px;
+      }
+    </style>
+  `;
+
+  return html`
+    <!-- begin artists -->
+    ${style}
+
+    <div id="artists">
+      ${artistsHtml}
+    </div>
+    <!-- end artists -->
+  `;
+}
